ecab0b0ca0b07c31cd028dc04c37f4ddb96fce36
dumitory-dev <blackmesa2018@gmail.com>
2021-12-01 19:08:46 +0200

    feat: add project src
    refactor: change keywords and categories description
    
    fix: invalid value 'get features'
    
    feat: added url repo
    
    fix: unused categories removed
    
    refactor: description
    
    feat: upload project

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn get_enabled_features(_cargo_file: TokenStream) -> TokenStream {
+    //https://www.reddit.com/r/rust/comments/834g53/list_of_conditional_compilation_features_at/
+    let features_gen_code = r#"
+        macro_rules! make_feat_list {
+            ($($feat:expr),*) => {
+                vec![ 
+                    $(
+                        #[cfg(feature = $feat)]
+                        $feat,
+                    )* 
+                ]
+            }
+        }
+        let enabled_features:Vec<&'static str> = make_feat_list!(FEATURES);
+    "#;
+
+    let metadata = cargo_metadata::MetadataCommand::new()
+        .manifest_path("./Cargo.toml")
+        .features(cargo_metadata::CargoOpt::AllFeatures)
+        .exec()
+        .unwrap();
+
+    //Get string of features
+    let root_packet_features = format!(
+        "{:?}",
+        metadata
+            .root_package()
+            .unwrap()
+            .features
+            .keys()
+            .cloned()
+            .collect::<Vec<String>>()
+    );
+
+    //Without '[' in the  begin and ']' in the end
+    let slice_features = &root_packet_features[1..root_packet_features.len() - 1];
+
+    let features_gen_code = features_gen_code.replace("FEATURES", slice_features);
+
+    features_gen_code.parse().unwrap()
+}
